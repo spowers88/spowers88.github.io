c82e31ba2bacdab1ba1c038279a8f84e9bf3d833
Sean Powers <spsimplicity@gmail.com>
2019-05-07 21:03:17 -0700

    fixing double nav

--- static/pages/algorithms.jsx
+++ static/pages/algorithms.jsx
@@ -1,14 +1,10 @@
 // Framework
 import React from 'react';
 
-// Components
-import Navigation from '../components/navigation/navigation';
-
 class Algorithms extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Navigation/>
         <h2>Algorithms</h2>
       </React.Fragment>
     );
--- static/pages/home.jsx
+++ static/pages/home.jsx
@@ -1,14 +1,10 @@
 // Framework
 import React from 'react';
 
-// Components
-import Navigation from '../components/navigation/navigation';
-
 class Home extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Navigation/>
         <h2>Home</h2>
       </React.Fragment>
     );
