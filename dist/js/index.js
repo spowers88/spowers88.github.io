71d6cb78300cd47b57f04270e01e5851bf248178
Sean Powers <spsimplicity@gmail.com>
2019-05-07 18:07:56 -0700

    adding dist back

---
+++ dist/js/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var _preact = require("/web_modules/preact.js");
+
+var _home = _interopRequireDefault(require("../pages/home"));
+
+var _algorithms = _interopRequireDefault(require("../pages/algorithms"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+// Framework
+// Pages
+(0, _preact.render)((0, _preact.h)(_home["default"], null), document.body);
