9d4e38edc13dcaaba2122251d011ddf8932c175b
Sean Powers <spsimplicity@gmail.com>
2019-05-14 23:12:50 -0700

    adding shadows and borders

--- dist/bundle.js
+++ dist/bundle.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "63c54482b234165b30c8";
+/******/ 	var hotCurrentHash = "19a26d9cfab0d6922016";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
