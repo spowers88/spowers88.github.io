33f05dcac3f5e76fb6c5da91615dcde522ef2bd9
Sean Powers <spsimplicity@gmail.com>
2019-05-16 12:18:56 -0700

    updating service worker

--- static/js/service-worker.js
+++ static/js/service-worker.js
@@ -1,46 +1,49 @@
+const IMAGE_CACHE_NAME = 'image-cache';
+const ASSETS_CACHE_NAME = 'assets-cache';
+let imageUrlsToCache = [
+  '/static/img/cbs-logo.png',
+  '/static/img/nerdwallet-logo.png',
+  '/static/img/brightwheel-logo.svg',
+  '/static/img/rocketlawyer-logo.png',
+  '/static/img/fullstack-react-native-gumroad.png'
+];
+let assetsUrlsToCache = [
+  '/',
+  '/dist/main.css',
+  '/dist/bundle.js'
+];
+
 self.addEventListener('install', (event) => {
-  const IMAGE_CACHE_NAME = 'image-cache';
-  const ASSETS_CACHE_NAME = 'assets-cache';
-  let imageUrlsToCache = [
-    '/static/img/cbs-logo.png',
-    '/static/img/nerdwallet-logo.png',
-    '/static/img/brightwheel-logo.svg',
-    '/static/img/rocketlawyer-logo.png',
-    '/static/img/fullstack-react-native-gumroad.png'
-  ];
-  let assetsUrlsToCache = [
-    '/dist/main.css',
-    '/dist/bundle.js'
-  ];
+  console.log('Installing service worker');
 
   event.waitUntil(
-    caches.open(ASSETS_CACHE_NAME)
-      .then(cache => cache.addAll(assetsUrlsToCache))
-  );
-  event.waitUntil(
-    caches.open(IMAGE_CACHE_NAME)
+    Promise.all([
+      caches.open(ASSETS_CACHE_NAME)
+      .then(cache => cache.addAll(assetsUrlsToCache)),
+      caches.open(IMAGE_CACHE_NAME)
       .then(cache => cache.addAll(imageUrlsToCache))
+    ])
   );
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys()
-      .then(cacheNames => {
-        const cachesToDelete = cacheNames.map((cacheName) => {
-          return caches.delete(cacheName);
-        });
-
-        return Promise.all(cachesToDelete);
-      })
-  );
+  console.log('Activating service worker');
 });
 
 self.addEventListener('fetch', (event) => {
+  console.log(event.request);
+  console.log('Fetch event for ', event.request.url);
+
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        return response || fetch(event.request);
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
+        }
+
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request);
       })
   );
 });
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        exclude: /node_modules|service-worker/,
         use: ['babel-loader']
       },
       {
